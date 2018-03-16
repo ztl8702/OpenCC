9afc0fb89edb46a65aa44eb067c4a62c0b7c22b4
Radium Zheng <ztl8702@msn.com>
2018-03-17 05:33:45 +1100

    finishing up wasm demo

--- src/wasm/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-em++ OpenCCTest.cpp \
-     ../../build/wasm/src/libopencc.a -O0 \
-     -o OpenCCTest.html \
-     -s WASM=1 -s ASSERTIONS=1 -s DISABLE_EXCEPTION_CATCHING=0 \
-     -std=c++11 \
-     -Wall --emrun \
-     --preload-file ../../build/linux32/data@data
