eb8b17025502044d6e4e22f184d89d461107dc21
Radium Zheng <ztl8702@msn.com>
2018-03-17 06:45:38 +1100

    demo web page

--- wasm/build.sh
+++ wasm/build.sh
@@ -4,6 +4,7 @@ em++ OpenCC.cpp \
      ../build/wasm/src/libopencc.a -O0 \
      -o opencc_bindings.js \
      -s WASM=1 -s ASSERTIONS=1 -s DISABLE_EXCEPTION_CATCHING=0 \
+     -s ALLOW_MEMORY_GROWTH=1 \
      -std=c++11 \
      -Wall \
      -O3 \
